f7c6b57a6e4afb29c0a3073f0e813bacc4a72c80
vishnu7674 <vishnulakky333@gmail.com>
2025-01-09 10:36:45 +0530

    dfsdf

--- vars/k8spipeline.groovy
+++ vars/k8spipeline.groovy
@@ -1,12 +1,14 @@
 import com.i27academy.builds.Calculator
 import com.i27academy.builds.Docker
-import com.i27academy.K8s.K8s
+import com.i27academy.k8s.K8s 
+
 
 def call(Map pipelineParams){
     // An instance of the class called calculator is created
     Calculator calculator = new Calculator(this)
     Docker docker = new Docker(this)  
     K8s k8s = new K8s(this)
+    
 
 // this Jenkins pipeline is for Eureka deployment
 
